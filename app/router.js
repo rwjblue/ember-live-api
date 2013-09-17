f8abd816d3775d9976b7796f93e42bdaf984ca7f
Robert Jackson <robert.w.jackson@me.com>
2013-09-17 09:37:11 -0400

    Add modules.show route.

--- app/router.js
+++ app/router.js
@@ -2,6 +2,10 @@ var router = Ember.Router.map(function(){
   this.resource('classes', function() {
     this.route('show', {path: '/:className'});
   });
+
+  this.resource('modules', function() {
+    this.route('show', {path: '/:moduleName'});
+  });
 });
 
 export default router;
