6c1d2452bbd9caa6c94a1fb30a9c270be65b0a74
Robert Jackson <robert.w.jackson@me.com>
2013-09-17 15:51:08 -0400

    Add highlighting to markdown.

---
+++ app/components/api-markdown.js
@@ -0,0 +1,57 @@
+var ApiMarkdown = Ember.Component.extend({
+  tagName: 'span',
+
+  markdown: function(){
+    var text = this.get('text'),
+        markdown = marked(text);
+
+    return new Ember.Handlebars.SafeString(markdown);
+  }.property('text'),
+
+  didInsertElement: function(){
+    var self = this;
+
+    this.$('pre code').each(function(index, element) {
+      var $element = $(element),
+          $parent  = $element.parent(),
+          code     = $element.text();
+      
+      $parent.replaceWith(self.formatCodeBlock(code));
+    });
+
+    this.$('td.code').each(function(index, element) {
+      hljs.highlightBlock(element);
+    });
+  },
+
+  formatCodeBlock: function(code){
+    var lines = code.split("\n"),
+        lineNumbers = '',
+        result;
+
+    lines.forEach(function(item, index){
+      var humanIndex = index + 1;
+
+      lineNumbers = lineNumbers + humanIndex + "\n";
+    });
+
+    result = '<div class="highlight">' + 
+             '  <div class="ribbon"></div>' +
+             '  <div class="scroller">' +
+             '    <table class="CodeRay">' +
+             '      <tr>' +
+             '        <td class="line-numbers">' +
+             '          <pre>' + lineNumbers + '</pre>' +
+             '        </td>' +
+             '        <td class="code"><pre>' + code + '</pre></td>'
+             '      </tr>' +
+             '    </table>' +
+             '  </div>' +
+             '</div>'
+
+    return result;
+  }
+
+});
+
+export default ApiMarkdown;
