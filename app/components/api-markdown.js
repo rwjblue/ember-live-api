0e550691d591b2747be990c84f79cfb05739b2e6
Robert Jackson <robert.w.jackson@me.com>
2013-09-19 21:06:30 -0400

    Remove need for afterRender jQuery operations.
    Monkey patched marked.js to generate the code blocks formatted
    how we need/want.

--- app/components/api-markdown.js
+++ app/components/api-markdown.js
@@ -1,33 +1,41 @@
 var ApiMarkdown = Ember.Component.extend({
   tagName: 'span',
 
+  overrideMarkedParser: function(){
+    var Parser = marked.Parser,
+        formatCodeBlock = this.formatCodeBlock;
+
+    if (Parser.prototype.overridden !== true){
+      Parser.prototype.tokOriginal = Parser.prototype.tok;
+      Parser.prototype.tok = function(){
+        if (this.token.type == 'code'){
+          var language = this.token.lang,
+              code     = this.token.text;
+
+          code = hljs.highlight(language, code).value;
+          return formatCodeBlock(code, language);
+        } else {
+          return this.tokOriginal();
+        }
+      };
+
+      Parser.prototype.overridden = true;
+    }
+  },
+
   markdown: function(){
+
     var self     = this,
         text     = this.get('text'),
-        markdown = marked(text);
+        markdown;
 
-    Ember.run.scheduleOnce('afterRender', this, this.updateCodeBlocks);
+    this.overrideMarkedParser();
+    markdown = marked(text)
 
     return new Ember.Handlebars.SafeString(markdown);
   }.property('text'),
 
-  updateCodeBlocks: function(){
-    var self = this;
-
-    this.$('pre code').each(function(index, element) {
-      var $element = $(element),
-          $parent  = $element.parent(),
-          code     = $element.text();
-      
-      $parent.replaceWith(self.formatCodeBlock(code));
-    });
-
-    this.$('td.code').each(function(index, element) {
-      hljs.highlightBlock(element);
-    });
-  },
-
-  formatCodeBlock: function(code){
+  formatCodeBlock: function(code, language){
     var lines = code.split("\n"),
         lineNumbers = '',
         result;
@@ -38,7 +46,7 @@ var ApiMarkdown = Ember.Component.extend({
       lineNumbers = lineNumbers + humanIndex + "\n";
     });
 
-    result = '<div class="highlight">' + 
+    result = '<div class="highlight ' + language + '">' +
              '  <div class="ribbon"></div>' +
              '  <div class="scroller">' +
              '    <table class="CodeRay">' +
@@ -46,7 +54,7 @@ var ApiMarkdown = Ember.Component.extend({
              '        <td class="line-numbers">' +
              '          <pre>' + lineNumbers + '</pre>' +
              '        </td>' +
-             '        <td class="code"><pre>' + code + '</pre></td>' +
+             '        <td class="code"><pre class="' + language + '">' + code + '</pre></td>' +
              '      </tr>' +
              '    </table>' +
              '  </div>' +
