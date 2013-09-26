7487529b76c528393151293d3e11157afa2d6c00
Robert Jackson <robert.w.jackson@me.com>
2013-09-26 16:51:23 -0400

    Add sidebar acceptance test.

---
+++ tests/acceptance/sidebar_test.js
@@ -0,0 +1,27 @@
+var App;
+
+module("Acceptances - Index", {
+  setup: function(){
+    var sampleDataUrl = '';
+
+    if (typeof __karma__ !== 'undefined')
+      sampleDataUrl = '/base';
+
+    sampleDataUrl = sampleDataUrl + '/tests/support/api.json';
+
+    App = startApp({apiDataUrl: sampleDataUrl});
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test("sidebar renders list of modules/classes", function(){
+  expect(2);
+
+  visit('/').then(function(){
+    debugger;
+    ok(exists("a:contains('Ember.Application')"));
+    ok(exists("a:contains('Ember.run')"));
+  });
+});
