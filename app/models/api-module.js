bb6d5f5c179a7146946c45e905b5bef114dfc399
Sam Selikoff <sam.selikoff@gmail.com>
2014-02-17 01:38:22 -0500

    First pass at Modules template

--- app/models/api-module.js
+++ app/models/api-module.js
@@ -1,4 +1,5 @@
 import Model from 'appkit/api-store/model';
+import { getModule } from 'appkit/api-store/helpers';
 
 var alias = Ember.computed.alias;
 
@@ -8,5 +9,24 @@ export default Model.extend({
   name:           alias('data.name'),
   description:    alias('data.description'),
   file:           alias('data.file'),
-  line:           alias('data.line')
+  line:           alias('data.line'),
+  requires:       alias('data.requires'),
+  parent:         getModule('data.module'),
+
+  classes: function() {
+    var _this = this;
+
+    return Object.keys(this.get('data.classes')).map(function(className) {
+      return _this.store.findClass(className);
+    });
+  }.property('data.classes'),
+
+  submodules: function() {
+    var _this = this;
+
+    return Object.keys(this.get('data.submodules')).map(function(name) {
+      return _this.store.findModule(name);
+    });
+  }.property('data.submodules')
+
 });
